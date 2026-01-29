2d31e2ef4ad2d0340df43d5afc12c6d1a9644142
anshxcode <goyalanshuman249@gmail.com>
2026-01-29 19:37:17 +0530

    feat: add basic structure

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, "public")));
+
+
+// app.get('/', (req, res) => {
+//     res.send('hiuuuyyuuu')
+// })
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.get("/profile/beta", (req, res) => {
+    res.send("beta");
+});
+
+
+app.listen(3000, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+    console.log("Server is running on port 3000");
+}});
